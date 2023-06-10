add91d6d175657558d6c0ea125f52bd8ee07959e
SergiiZInkovskii <zsircoz@gmail.com>
2023-06-10 13:28:02 +0300

    publish

--- Патерни/Adapter/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using Adapter;
-
-float kg = 55;
-float lb = 55;
-IScales euroScales = new EuroScales(kg);
-IScales britishScales = new AdapterForBritishScales(new BritishScales(lb));
-Console.WriteLine(euroScales.GetWeight() + "kg");
-Console.WriteLine(britishScales.GetWeight()+ "kg");
